f160412ce493ef8f6a2946ace3af1ce0a924d6fd
vishwajeet kumar <vishusrivastva@users.noreply.github.com>
2024-03-25 20:23:39 +0530

    added lower case button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   const handleUpClick = () => {
     console.log("Upercase button clicked");
     let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLowClick = () => {
+    console.log("lowercase button clicked");
+    let newText = text.toLowerCase();
     setText(newText);
   };
   const handleOnChange = (event) => {
@@ -11,20 +16,34 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
   return (
-    <div>
-      <div className="mb-3">
-        <h1>{props.heading}</h1>
-        <textarea
-          className="form-control"
-          value={text}
-          id="myBox"
-          rows="8"
-          onChange={handleOnChange}
-        ></textarea>
+    <>
+      <div>
+        <div className="container mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="8"
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Covert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          Covert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Covert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h2>Your Text Summmary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }

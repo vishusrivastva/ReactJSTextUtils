431ed5762d4658d4d1589335f48bd4e934aa476e
vishwajeet kumar <vishusrivastva@users.noreply.github.com>
2024-03-25 21:40:51 +0530

    added dark mode and copy and extra space reover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar title="TextUtils" aboutText="About" />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
+        {/* <About></About> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,6 +16,15 @@ export default function TextForm(props) {
     let newText = "";
     setText(newText);
   };
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handleExtaSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
   const handleOnChange = (event) => {
     console.log("on Change");
     setText(event.target.value);
@@ -41,6 +50,12 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>
           Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtaSpaces}>
+          Remove Extra Spaces
         </button>
       </div>
       <div className="container my-3">
